a33e93664d7f2cc8a0149780662739e03f8b3a2b
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-04-03 13:21:36 +0530

    feat: create todos page layout

---
+++ todo-frontend/app/todos/layout.tsx
@@ -0,0 +1,17 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+const TodosLayout = async ({children}: React.ReactNode) => {
+    const token =  (await cookies()).get("token")?.value
+
+    if(!token){
+        redirect('/')
+    }
+
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default TodosLayout
