69366e26d4628e44da544b593c43d3759184f88b
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-04-03 10:24:51 +0530

    fix: cors error

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const app = require("./src/app");
 const connectDB = require("./db/connectDB");
 
+const port = 4000
+
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(3000, () => {
-      console.log("server started");
+    app.listen(port, () => {
+      console.log("server started", port);
     });
   } catch (error) {
     console.error("Server failed to start", error);
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,21 @@
 require("dotenv").config();
+const cors = require("cors")
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const todoRoutes = require("../Routes/todo.routes");
 const userRoutes = require("../Routes/user.routes");
 
 const app = express();
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use("/api/todos", todoRoutes);
 
-app.use("/api/users", userRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
