0f8835d0e23bcf06f863e1c485d8b3d46b5a2145
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-04-03 13:21:48 +0530

    feat: create todos page client component

---
+++ todo-frontend/components/custom/TodosClient.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useState } from "react"
+import Header from "./Header"
+import TasksList from "./TasksList"
+import TodosSidebar from "./TodosSidebar"
+
+const TodosClient = () => {
+  const [filter, setFilter] = useState<'pending' | 'completed'>('pending')
+
+  return (
+    <>
+      <Header />
+
+      <div className='grid grid-cols-3 gap-6 h-[calc(100%-80px)]'>
+        <TodosSidebar filter={filter} setFilter={setFilter} />
+        <TasksList filter={filter} />
+      </div>
+    </>
+  )
+}
+
+export default TodosClient
