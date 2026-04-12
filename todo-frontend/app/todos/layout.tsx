6e44c3823b8e9ef4250d0498ac43bb54e3cb26be
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-04-12 13:09:27 +0530

    fix(todos): remove token validation and redirect logic from TodosLayout

--- todo-frontend/app/todos/layout.tsx
+++ todo-frontend/app/todos/layout.tsx
@@ -1,17 +1,5 @@
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-import React from 'react'
+const TodosLayout = async ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
 
-const TodosLayout = async ({children}: { children: React.ReactNode }) => {
-    const token =  (await cookies()).get("token")?.value
-
-    if(!token){
-        redirect('/')
-    }
-
-  return (
-    <div>{children}</div>
-  )
-}
-
-export default TodosLayout
+export default TodosLayout;
